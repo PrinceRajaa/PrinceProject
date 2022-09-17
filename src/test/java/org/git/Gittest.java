80e6f83c06e125373d0be610ad23db967d2e046e
PrinceRajaa <raja876@yahoo.com>
2022-09-17 19:06:41 +0530

    Frist time push

---
+++ src/test/java/org/git/Gittest.java
@@ -0,0 +1,34 @@
+package org.git;
+
+
+
+public class Gittest {
+	public static void main(String[] args) {
+		   Gittest code = new Gittest();
+		   code.empId();
+		   code.empName();
+		   code.empDob();
+		   code.empPhone();
+		   code.empEmail();
+		   code.empAddress();
+	}
+		private void empId() {
+		System.out.println("Employee Id");
+	}
+	private void empName() {
+		System.out.println("Employee Name");
+	} 
+	  private void empDob() {
+		  System.out.println("Employee Dob");
+	}
+	  private void empPhone() {
+	    System.out.println("Employee Phone");
+	}
+	  private void empEmail() {
+	     System.out.println("Employee Email");
+	}
+	  private void empAddress() {
+	    System.out.println("Employee Address");
+	}
+
+}
